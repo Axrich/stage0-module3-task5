6f392fdf7b322772d6fe15aee6f87aab8e666b4c
Axrich <116589250+Axrich@users.noreply.github.com>
2023-06-09 15:06:12 +0500

    Update ThreeStuckVars.java

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        int i = 1;
+        int j = 10;
+        int k= 100;
+        System.out.println(i+ " " + j + " " + k);
     }
 }
